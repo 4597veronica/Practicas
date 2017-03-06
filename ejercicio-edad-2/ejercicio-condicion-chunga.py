3d0550665aa229951368f75f674ee986e25a64b9
Veronica <4597veronica@gmail.com>
2017-03-06 21:31:10 +0100

    Create ejercicio-condicion-chunga.py

---
+++ ejercicio-edad-2/ejercicio-condicion-chunga.py
@@ -0,0 +1,6 @@
+#coding:utf-8
+numero=input("Indique número:")
+if (numero %2==0 and numero <0) and (numero >=-10 and numero <=40) :
+    print "Que bonito número"
+else : 
+    print "Que número más vulgar"
