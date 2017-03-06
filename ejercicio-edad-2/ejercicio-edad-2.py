21d638c72745ae7b58cf69c7d98eb190e1604ebe
Veronica <4597veronica@gmail.com>
2017-03-06 21:45:50 +0100

    Update ejercicio-edad-2.py

--- ejercicio-edad-2/ejercicio-edad-2.py
+++ ejercicio-edad-2/ejercicio-edad-2.py
@@ -2,4 +2,4 @@
 edad=input ("Indique su edad")
 if edad >=18 and edad <=23 or edad==17:
   print "Puedes pasar"
-  print "Te pertenece la sesión de jóvenes"
+  print "Te pertenece la sesion de jovenes"
