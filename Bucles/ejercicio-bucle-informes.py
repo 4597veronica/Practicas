ef25dc4501f1c20a0b4d021e9d4f787ddcfc4745
Veronica <4597veronica@gmail.com>
2017-03-21 11:30:05 +0100

    Update ejercicio-bucle-informes.py

--- Bucles/ejercicio-bucle-informes.py
+++ Bucles/ejercicio-bucle-informes.py
@@ -1,8 +1,9 @@
 # coding:utf-8
+#Vijular donde se incrementa
 sortir=False
 anyo = 2010
 while sortir== False:
 	print "Informe de",anyo
-	anyo=anyo+1
-	if contador == 2017:
+	if contador == 2016:
 		sortir = True
+	anyo=anyo+1 #todos los incrementos siempre al final
