fb9ae2b17703a37bc847813944d191c7b6e45fba
Veronica <4597veronica@gmail.com>
2017-03-21 11:31:52 +0100

    Update and rename while_plantilla to while_plantilla.py

--- Bucles/while_plantilla.py
+++ Bucles/while_plantilla.py
@@ -1,8 +1,8 @@
 # coding:utf-8
-#Esto es una plantilla de un bucle
+#Esto es una plantilla de un bucle: Mientras que no se introduzzca Xs
 sortir=False
 while sortir== False:
   print("Hola")
   tecla=raw_input("Introdice una x para salir ")
   if tecla =="x" or tecla=="X":
-    sortir = True
+    sortir = True 
