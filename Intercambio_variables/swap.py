faebf5b21fa961db2580c27ad25bc736d6b529ec
Veronica <4597veronica@gmail.com>
2017-03-21 11:22:21 +0100

    Create swap.py

---
+++ Intercambio_variables/swap.py
@@ -0,0 +1,31 @@
+# coding: utf­8
+#En este programa haremos un intercambio de variables
+mano_der="movil"
+mano_izq="bocadillo"
+mano_extra="nada"
+
+print"Primer cambio"
+print mano_der
+print mano_izq
+print""
+
+mano_extra=mano_der
+
+print"Segundo cambio"
+
+print mano_der
+print mano_izq
+print""
+
+mano_der=mano_izq
+
+
+print"Tercer cambio"
+print mano_der
+print mano_izq
+print""
+mano_izq=mano_extra
+
+print"Cuarto cambio"
+print mano_der
+print mano_izq
