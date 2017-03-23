a47e11d8e140be4b6766c6bac82b962ef782fc30
Veronica <4597veronica@gmail.com>
2017-03-23 22:35:03 +0100

    Create ejercicio-mayor.py

---
+++ ejercico-mayor/ejercicio-mayor.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+
+#En este codigo coimpararemos dos numeros
+
+numero1=input ("Introduce el primer numero: ")
+
+numero2=input ("Introduce el segundo numero: ")
+
+if numero1<numero2:
+	print "El numero", numero2, "es mas grande que el numero" ,numero1
+else :
+	if numero1>numero2:
+		print "El numero", numero1, "es mas grande que el numero" ,numero2
+	else: 
+		print "Los numeros son iguales"
