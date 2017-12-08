e818d3a5e17d0ad70e4a931d1e170ee9debfef85
Veronica <4597veronica@gmail.com>
2017-12-08 16:09:56 +0100

    Add files via upload

---
+++ Bucles/pyt05.py
@@ -0,0 +1,18 @@
+#coding:utf8
+
+limite = input("Introduce un número límite, por favor: ")
+
+num = 1
+
+while num <= limite :
+
+    print str(num)
+    
+    #str= string, cadena
+    
+    num = num + 1
+print "Fin del programa."
+
+#Va sumando +1 a num que es 1 hasta que llega al número indicado en límite
+#que es cuando va a terminar el bucle.
+#Cuenta hasta el límite que yo le digo.
